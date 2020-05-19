76d15e11976c9763a2e482ad93632cf07267887d
Maja Gach <majaanna94@gmail.com>
2020-05-19 13:17:54 +0200

    deploy confi

--- back/server.js
+++ back/server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 app.use(cors());
+const path = require('path');
 
 const shows = require('./routes/api/shows');
 const messages = require('./routes/api/messages');
@@ -22,6 +23,13 @@ mongoose
 
 app.use('/contact', messages);
 app.use('/shows', shows);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
